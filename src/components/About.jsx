bfef0edb656f3926341efa86b5cc302105c117ba
ali-shah007 <alishah69.ah@gmail.com>
2024-05-21 11:52:47 +0500

    responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function About() {
   return (
     <div name="about" className=' w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white'>
-        <div className=' max-w-screen-lg p-4 max-auto flex flex-col justify-center w-full h-full'>
+        <div className=' max-w-screen-lg p-4 max-auto flex flex-col justify-center w-full h-full lg:px-20'>
             <div className=' pb-8'>
                 <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
 
             </div>
-            <p className=' text-xl mt-20'>Welcome to my portfolio! I'm a front-end web developer specializing in HTML, CSS, JavaScript, and React. With a keen eye for design and functionality, I craft engaging and responsive web experiences that captivate users.</p>
+            <p className=' text-xl mt-10'>Welcome to my portfolio! I'm a front-end web developer specializing in HTML, CSS, JavaScript, and React. With a keen eye for design and functionality, I craft engaging and responsive web experiences that captivate users.</p>
             <br/>
             <p className='text-xl'>In addition to my expertise in front-end development, I'm proficient in Python and utilize it for various tasks, including web scraping. Leveraging Python, I generate leads for businesses by extracting valuable data from the web. Whether it's aiding in product listing or providing support for web projects, I'm committed to delivering high-quality solutions tailored to meet your needs.</p>
             <br/>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-scroll'
 function Home() {
   return (
     <div name="home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className=' max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full px-2'>
+        <div className=' max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 sm:flex-row'>
+            <div className='flex flex-col justify-center h-full px-2 mt-10'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
                     I'm a Front-End Web Developer and Web Scrapper
                 </h2>
@@ -27,7 +27,7 @@ function Home() {
                 </div>
             </div>
             <div>
-                <img src={HeroImage} alt="my profile" className=' rounded-2xl mx-auto w-2/3 md:w-full'/>
+                <img src={HeroImage} alt="my profile" className=' rounded-3xl mx-auto w-2/3 sm:w-full mb-10'/>
             </div>
         </div>
     </div>
