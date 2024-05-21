3314875d2acf8f21f8743f36723bc354b4ecb8c3
ali-shah007 <alishah69.ah@gmail.com>
2024-05-21 17:06:04 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Experience from "./components/Experience";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import Portfolio from "./components/Portfolio";
-import SocialLinks from "./components/SocialLinks";
+
+import LoadingComponent from "./components/LoadingComponent";
+
+
 
 function App() {
   return (
    <div>
-    <NavBar />
-    <Home />
-    <About />
-    <Portfolio />
-    <Experience />
-    <Contact />
-
-
-    <SocialLinks/>
+    <LoadingComponent/>
    </div>
   );
 }
